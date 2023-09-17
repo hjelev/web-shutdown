8c76301a59347be7be816fb6db9fba7da837ce25
Hristo <jeleff@gmail.com>
2023-09-17 12:16:00 +0300

    go version

--- web-shutdown.py
+++ web-shutdown.py
@@ -1,22 +1,36 @@
-import bottle
-import os
 import platform
-
-@bottle.route('/shutdown', method='POST')
-def shutdown():
-    system = platform.system()
-    if system == 'Windows':
-        os.system('shutdown /s /t 0')
-    elif system == 'git a' or system == 'Linux':
-        os.system('shutdown now')
-    else:
-        return "Shutdown not supported on this operating system."
+import subprocess
+import bottle
 
 @bottle.route('/')
 def home():
-    server_ip = bottle.request.urlparts.netloc.split(':')[0]
-    return f"Make a POST request to http://{server_ip}/shutdown to shutdown the server."
+    message = "Make a POST request to /shutdown to shutdown the PC\n" \
+              "Make a POST request to /wakeup to wake up the monitor"
+    return message
 
+@bottle.route('/shutdown', method='POST')
+def shutdown():
+    operating_system = platform.system().lower()
+    if 'windows' in operating_system:
+        subprocess.call('shutdown /s /t 0', shell=True)
+    elif 'darwin' in operating_system or 'linux' in operating_system:
+        subprocess.call('sudo shutdown -h now', shell=True)
+    else:
+        return bottle.HTTPResponse(status=500, body='Shutdown not supported on this operating system.')
+    return 'Shutdown request received. The PC will shut down shortly.'
+
+@bottle.route('/wakeup', method='POST')
+def wakeup():
+    operating_system = platform.system().lower()
+    if 'windows' in operating_system:
+        subprocess.call('powershell -c "(New-Object -ComObject Shell.Application).ToggleDesktop()"', shell=True)
+    elif 'darwin' in operating_system:
+        subprocess.call('caffeinate -u -t 1', shell=True)
+    elif 'linux' in operating_system:
+        subprocess.call('xset dpms force on', shell=True)
+    else:
+        return bottle.HTTPResponse(status=500, body='Monitor wake not supported on this operating system.')
+    return 'Monitor wake request received. The monitor will be woken up shortly.'
 
 if __name__ == '__main__':
     bottle.run(host='0.0.0.0', port=4321)
